57d701c45a1d5ca95eaea1fbe28aae209c173412
JoseHenriiquep <josehenriquenetinho@gmail.com>
2025-03-21 15:55:05 -0300

    dir

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Garante que a saída será no diretório 'build'
+  },
 })
